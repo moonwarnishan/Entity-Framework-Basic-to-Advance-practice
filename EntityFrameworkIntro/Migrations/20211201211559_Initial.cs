f0c7c7583e439c4106eac256287003198b20c8f3
unknown <nishan.rumc@gmail.com>
2021-12-02 05:06:02 +0600

    Basic part

---
+++ EntityFrameworkIntro/Migrations/20211201211559_Initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkIntro.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Star = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UnitOfMeasure = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    DishId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishIngredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DishIngredients_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishIngredients_DishId",
+                table: "DishIngredients",
+                column: "DishId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DishIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+        }
+    }
+}
---
+++ EntityFrameworkIntro/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+var factory = new CookBookContextFactory();
+using var context = factory.CreateDbContext(args);
+
+
+// Console.WriteLine("Add some data");
+// var bf1 = new Dish {Title = "breakfast",Notes = "this is so good", Star = 5};
+// var bf2 = new Dish { Title = "eakfast", Notes = "is so good", Star = 6 };
+// var bf3 = new Dish { Title = "east", Notes = "isgood", Star = 7 };
+// context.Dishes.Add(bf1);
+// context.Dishes.Add(bf2);
+// context.Dishes.Add(bf3);
+//  await context.SaveChangesAsync();
+// Console.WriteLine("added");
+//Console.WriteLine("remove");
+//context.Dishes.Remove(bf);
+//await context.SaveChangesAsync();
+//Console.WriteLine("removed");
+//bf.Star = 6;
+//await context.SaveChangesAsync();
+
+var dis = context.Dishes
+    .Where(d => d.Star == 6)
+    .ToList();
+foreach (var dish in dis)
+{
+    Console.WriteLine($"{dish.Title} {dish.Star}");
+}
+
+
+
+class Dish
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+
+    //can be null so use ?
+    [MaxLength(100)]
+    public string? Notes { get; set; }
+    public int? Star { get; set; }
+
+    //a dish can have multiple dish ingredient
+    public List<DishIngredient> Ingredients { get; set; } = new();
+}
+
+class DishIngredient
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+    [MaxLength(50)]
+    public string UnitOfMeasure { get; set; }=string.Empty;
+    [Column(TypeName = "decimal(5,2)")]
+    public decimal Amount { get; set; }
+    
+    //A ingredient can have Dish
+    public Dish? Dish { get; set; }
+    //forgien key
+    public int DishId { get; set; }
+}
+
+
+class CookBookContext: DbContext
+{
+#pragma warning disable CS8618
+    public CookBookContext(DbContextOptions<CookBookContext> options)
+#pragma warning restore CS8618
+        :base(options)
+    {
+        
+    }
+
+    public DbSet<Dish> Dishes { get; set; }
+    public DbSet<DishIngredient> DishIngredients { get; set; }
+
+
+
+}
+
+class CookBookContextFactory:IDesignTimeDbContextFactory<CookBookContext>
+{
+    public CookBookContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<CookBookContext>();
+        optionsBuilder
+            // Uncomment the following line if you want to print generated
+            // SQL statements on the console.
+            .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+            .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        return new CookBookContext(optionsBuilder.Options);
+    }
+}
